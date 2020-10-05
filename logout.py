c14a48399bea09db124a81c2c2d90ce5d4bac4f0
mohsen <mohsenzarein1379@gmail.com>
2020-10-05 17:54:42 +0330

    add logout

---
+++ logout.py
@@ -0,0 +1,34 @@
+from login import Login
+from login import ClientError
+import sys
+import argparse
+
+
+def Logout(api):
+
+    try:
+        api.logout()
+        print("Logged out successfuly !")
+    except ClientError as err:
+        print(err)
+        sys.exit()
+        
+
+if __name__ == "__main__":
+
+    # Example command:
+    # python examples/savesettings_logincallback.py -u "yyy" -p "zzz" -settings "test_credentials.json"
+    parser = argparse.ArgumentParser(description='INSTAGRAM ROBOT')
+    parser.add_argument('-settings', '--settings', dest='settings_file_path', type=str, required=True)
+    parser.add_argument('-u', '--username', dest='username', type=str, required=True)
+    parser.add_argument('-p', '--password', dest='password', type=str, required=True)
+
+    args = parser.parse_args()
+
+    api = Login(
+                settings_file_path=args.settings_file_path,
+                username=args.username,
+                password=args.password
+    )
+
+    Logout(api)
